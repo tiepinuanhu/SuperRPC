94900b498df9b450341f6e8b093480a90b82daf3
WangXinchao <wangxinchao.cc@gmail.com>
2025-10-13 21:16:19 +0800

    no bug

---
+++ test-server/src/main/java/org/wxc/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package org.wxc.service;
+
+import org.wxc.api.User;
+import org.wxc.api.UserService;
+
+/**
+ * @author wangxinchao
+ * @date 2025/10/13 21:11
+ */
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getUser(Long id) {
+        return null;
+    }
+}
